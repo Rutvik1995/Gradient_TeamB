697b3d6e8f8ca66ab3be51cb6ba484a2143b771c
Rutvik <rutvikpatel.rm@gmail.com>
2019-03-04 17:05:53 -0800

    updated GradientTest.java

--- src/app/GradientTest.java
+++ src/app/GradientTest.java
@@ -413,6 +413,39 @@ class GradientTest
 		return result;
 	}
  
+ 
+ 
+ 
+ @Test
+	public void testIllegalException() {
+		assertThrows( IllegalArgumentException.class, () -> { Grade grade =new Grade("",9.0); } );
+	}
+	
+	@Test
+	public void compareC1()
+	{
+		Grade grade1 = new Grade("H.W",10.0);
+		Grade grade2 = new Grade("H.W",null);
+		assertEquals("",-1,grade2.compareTo(grade1));
+	}
+	@Test
+	public void compareC2() 
+	{
+		
+		Grade grade3 = new Grade("H.W",null);
+		Grade grade4 = new Grade("H.W",null);
+		assertEquals(0,grade3.compareTo(grade4));	
+	}
+	@Test
+	public void compareC3() 
+	{
+		Double b = null;
+		Double c= null;
+		Grade grade5 = new Grade("H.W",null);
+		Grade grade6 = new Grade("H.W",10.0);
+		assertEquals(-1,grade5.compareTo(grade6));
+	}
+ 
 
 }
 
