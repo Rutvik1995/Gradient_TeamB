7ca801175004d3a38f06be8b180612efb3e93043
Rutvik <rutvikpatel.rm@gmail.com>
2019-03-04 17:04:46 -0800

    updated Grdae java filr

--- src/grading/Grade.java
+++ src/grading/Grade.java
@@ -3,7 +3,8 @@ import java.text.DecimalFormat;
 //import java.util.HashMap;
 //import java.util.List;
 
-public class Grade {
+public class Grade 
+{
 
 	//HashMap<String, Double> Grade = new HashMap<>();
 	private String key; //final
@@ -80,32 +81,36 @@ public class Grade {
 			//return 0;
 	}
 
-	@SuppressWarnings("unused")
-	public String toString()
-	{
-		Double val = new Double(this.value);
-		DecimalFormat dval = new DecimalFormat("#####.#");
-		dval.format(val);
-		if(dval != null)
-		{
-			// field of width 5 add 1 decimal to the right - modify value 
-			String formatString = this.key + ": " + dval;
-			//formatString.substring(0, 4);
-			return formatString;
-		}
-		else
-		{
-			//test to check if max this.key length to set to fixed 2 if not trim and then add static 3 length
-			StringBuilder sb = new StringBuilder();
-			sb.setLength(5);
-			sb.append(this.key + ": " + " NA");
-			//sb.append(" NA");
-			return sb.toString();
-			/*sb.setLength(5);
-			String formatString = this.key + ": " + sb.toString();
-			return formatString;*/
-		}
+	
+	
+	
+	
+//	@SuppressWarnings("unused")
+//	public String toString()
+//	{
+//		Double val = new Double(this.value);
+//		DecimalFormat dval = new DecimalFormat("#####.#");
+//		dval.format(val);
+//		if(dval != null)
+//		{
+//			// field of width 5 add 1 decimal to the right - modify value 
+//			String formatString = this.key + ": " + dval;
+//			//formatString.substring(0, 4);
+//			return formatString;
+//		}
+//		else
+//		{
+//			//test to check if max this.key length to set to fixed 2 if not trim and then add static 3 length
+//			StringBuilder sb = new StringBuilder();
+//			sb.setLength(5);
+//			sb.append(this.key + ": " + " NA");
+//			//sb.append(" NA");
+//			return sb.toString();
+//			/*sb.setLength(5);
+//			String formatString = this.key + ": " + sb.toString();
+//			return formatString;*/
+//		}
 		
 	}
 
-}
+
