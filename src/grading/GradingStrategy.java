834dde4a02e3ded2fdeb0821b0f4ec6e12bb3b09
Rishab <rishabkamshetty@gmail.comy>
2019-03-06 12:56:08 -0800

    Few changes in SizeException and GradingStrategy files

--- src/grading/GradingStrategy.java
+++ src/grading/GradingStrategy.java
@@ -1,9 +1,10 @@
 package grading;
-
 import java.util.List;
+
 /*
  * Interface for GradingStrategy
  */
+
 public interface GradingStrategy {	
 	
 	public Grade calculate(String courseWeight, List<Grade> grades) throws SizeException;
