530bd95f04548e6534b1047fa831a14608efa545
Neha Maiya <maiya.neha@gmail.com>
2019-03-06 20:40:20 -0800

    Implement Filter Interface

---
+++ src/grading/Filter.java
@@ -0,0 +1,16 @@
+package grading;
+
+import java.util.List;
+
+public interface Filter {
+
+	public static List<Grade> apply(List<Grade> grades) throws SizeException {
+		// TODO Auto-generated method stub
+		return grades;
+		
+	}
+
+}
+
+
+
